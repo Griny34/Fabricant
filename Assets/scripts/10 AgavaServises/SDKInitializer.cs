73107b99cdab21ff6c11d384bdda562ab06da2c7
Griny34 <43299@mail.ru>
2024-08-16 06:50:50 +0300

    Finish

--- Assets/scripts/10 AgavaServises/SDKInitializer.cs
+++ Assets/scripts/10 AgavaServises/SDKInitializer.cs
@@ -1,25 +1,35 @@
 using Agava.YandexGames;
+using Lean.Localization;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SDKInitializer : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _loadText;
+    [SerializeField] private string _editorLanguageOnInit = "Russian";
+
     private void Awake()
     {
         if (Agava.WebUtility.WebApplication.IsRunningOnWebGL == false)
+        {
+            LeanLocalization.SetCurrentLanguageAll(_editorLanguageOnInit);
+            SceneManager.LoadScene("MenuScene");
             return;
+        }      
 
         YandexGamesSdk.CallbackLogging = true;
+        _loadText.enabled = false;
     }
 
     private IEnumerator Start()
     {
-        if (Agava.WebUtility.WebApplication.IsRunningOnWebGL == false)
-        {
-            yield break;
-        }           
+        //if (Agava.WebUtility.WebApplication.IsRunningOnWebGL == false)
+        //{
+        //    yield break;
+        //}           
 
         Debug.Log("start");
         yield return YandexGamesSdk.Initialize(Initializer);
@@ -27,8 +37,17 @@ public class SDKInitializer : MonoBehaviour
 
     private void Initializer()
     {
-        if (Agava.WebUtility.WebApplication.IsRunningOnWebGL == false)
-            return;
+        //if (Agava.WebUtility.WebApplication.IsRunningOnWebGL == false)
+        //    return;
+
+        LeanLocalization.SetCurrentLanguageAll(YandexGamesSdk.Environment.i18n.lang switch
+        {
+            "ru" => "Russian",
+            "tr" => "Turkish",
+            "en" => "English"
+        });
+
+        _loadText.enabled = true;
 
         SceneManager.LoadScene("MenuScene");
     }
